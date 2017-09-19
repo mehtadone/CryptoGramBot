adf2100c4610cffc1299174c029e2b47eab75bcc
Chirag Mehta <mehtadone@hotmail.com>
2017-09-19 10:19:24 +0100

    namespace correction

--- CryptoGramBot/EventBus/Handlers/TradeNotificationHandler.cs
+++ CryptoGramBot/EventBus/Handlers/TradeNotificationHandler.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CryptoGramBot.EventBus;
-using CryptoGramBot.EventBus.Handlers;
+﻿using System.Threading.Tasks;
 using CryptoGramBot.Models;
 using Enexure.MicroBus;
 
-namespace CryptoGramBot.Services
+namespace CryptoGramBot.EventBus.Handlers
 {
     public class TradeNotificationCommand : ICommand
     {
